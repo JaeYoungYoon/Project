626c37d35769b9d8c3c1c311bfefafd5a03360ac
Jane <jy345678@naver.com>
2023-03-23 15:16:56 +0900

    구구단 내용 변경 (thead tbody)

--- 03 BootStrap과 jQuery를 이용하여 게임 만들기/game/js/gugudan.js
+++ 03 BootStrap과 jQuery를 이용하여 게임 만들기/game/js/gugudan.js
@@ -1,25 +1,27 @@
-$(document).ready(function () {
-  var table = $("<table>").addClass("table table-bordered table-primary");
-  var headerRow = $("<tr>");
-  headerRow.append($("<td>"));
+        $(function () {
+            var table = $("<table>").attr("border", "1").addClass("table table-bordered table-success"); // addClass
+            var headerRow = $("<thead>");
+            headerRow.append($("<td>"));
 
-  for (var i = 2; i <= 9; i++) {
-    var headerCell = $("<th>").text(i + "단");
-    headerRow.append(headerCell);
-  }
-  table.append(headerRow);
+            for (var i = 2; i <= 9; i++) {
+                var headerCell = $("<th>").text(i + "단");
+                headerRow.append(headerCell);
+            }
+            table.append(headerRow);
 
-  for (var j = 1; j <= 9; j++) {
-    var row = $("<tr>");
-    var firstCell = $("<td>").text(j);
-    row.append(firstCell);
+            var bodyRow = $("<tbody>");
+            for (var j = 1; j <= 9; j++) {
+                var row = $("<tr>");
+                var firstCell = $("<td>").text(j);
+                row.append(firstCell);
+                for (var i = 2; i <= 9; i++) {
+                    var cell = $("<td>").text(i + " x " + j + " = " + i * j);
+                    row.append(cell);
+                }
+                bodyRow.append(row);
+            }
 
-    for (var i = 2; i <= 9; i++) {
-      var cell = $("<td>").text(i + " x " + j + " = " + i * j);
-      row.append(cell);
-    }
-    table.append(row);
-  }
 
-  $("#gugudan").append(table);
-});
+            table.append(bodyRow);
+            $("#gugudan").append(table);
+        });
